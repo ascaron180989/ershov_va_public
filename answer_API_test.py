2e77e794be4894b524547da7dbad01a54790a1fc
Vyacheslav Ershov <94031513+ascaron180989@users.noreply.github.com>
2022-03-18 10:09:24 +0800

    Update answer_API_test.py
    Переписал в более понятный для восприятия вид.

--- answer_API_test.py
+++ answer_API_test.py
@@ -12,92 +12,81 @@
 import json
 
 
-def decorator_type(func):
-    def wrapper(*args):
-        result = str(func(*args))
-        if 'int' in result:
-            return 'int'
-        if 'str' in result:
-            return 'str'
-        if 'bool' in result:
-            return 'bool'
-    return wrapper
+def type_int(item):
+    return isinstance(item, int)
 
 
-# Проверка наличия обязательных полей
-def req_fields(answer: dict) -> list:
-    missing_field = []
-    count = 0
-    for i in required_fields:
-        if i in answer:
-            count += 1
-        else:
-            missing_field.append(i)
-    return [] if count == 16 else missing_field
+def type_str(item):
+    return isinstance(item, str)
 
 
-# Прверка наличия лишних полей
-def false_fields(answer: dict) -> list:
-    extra_field = []
-    for i in answer:
-        if i not in required_fields:
-            extra_field.append(i)
-    return [] if not extra_field else extra_field
+def type_bool(item):
+    return isinstance(item, bool)
 
 
-# Проверка полей: тип, URL, значение
-def type_fields(answer: dict) -> dict:
-    error_field = {}
-    for key, ans_i in answer.items():
-        if key in required_fields:
-            for req_i in required_fields[key]:
-
-                #  Проверка типа
-                if req_i in ['int', 'str', 'bool']:
-                    if type(ans_i) != req_i:
-                        if key not in error_field:
-                            error_field[key] = ['Не верный тип данных']
-                        else:
-                            error_field[key].append('Не верный тип данных')
-
-                #  Проверка URL
-                elif req_i == 'url':
-                    if 'http://' not in str(ans_i) and 'https://' not in str(ans_i):
-                        if key not in error_field:
-                            error_field[key] = ['Не верный формат ссылки URL']
-                        else:
-                            error_field[key].append('Не верный формат ссылки URL')
-
-                #  Проверка вхождения значения
-                else:
-                    if str(ans_i) not in required_fields[key][1:]:
-                        if key not in error_field:
-                            error_field[key] = ['Не верное значение']
-                        else:
-                            error_field[key].append('Не верное значение')
-                    break
-    return error_field
+def type_url(item):
+    if isinstance(item, str):
+        if 'http://' in item or 'https://' in item:
+            return True
+    return False
 
 
-type = decorator_type(type)
+def type_val(item, val):
+    if isinstance(item, str):
+        if item in val:
+            return True
+    return False
+
 
 # Словарь поле: значение, которые могут принемать соответсвующие поля
-required_fields = {"timestamp": ['int'], "referer": ['str', 'url'], "location": ['str', 'url'], "remoteHost": ['str'],
-                   "partyId": ['str'], "sessionId": ['str'], "pageViewId": ['str'],
-                   "eventType": ['str', 'itemBuyEvent', 'itemViewEvent'], "item_id": ['str'],
-                   "item_price": ['int'], "item_url": ['str', 'url'], "basket_price": ['str'],
-                   "detectedDuplicate": ['bool'], "detectedCorruption": ['bool'], "firstInSession": ['bool'],
-                   "userAgentName": ['str']}
+required_fields = {"timestamp": 'int', "referer": 'url', "location": 'url', "remoteHost": 'str', "partyId": 'str',
+                   "sessionId": 'str', "pageViewId": 'str', "eventType": 'val', "item_id": 'str',
+                   "item_price": 'int', "item_url": 'url', "basket_price": 'str', "detectedDuplicate": 'bool',
+                   "detectedCorruption": 'bool', "firstInSession": 'bool', "userAgentName": 'str'}
+
+# Списки ошибок
+extra_fields = []
+error_fields = []
+missing_fields = []
 
 
-#  Чтение ответа API
+# Чтение ответа API
 with open('answer_API.json', encoding='utf8') as answer_json:
     answers = json.load(answer_json)
 
-# Проверка и вывод
-for item in range(len(answers)):
-    print(f'Ответ №{item + 1}')
-    print('Отсутствуют поля:', req_fields(answers[item]))
-    print('Лишние поля:', false_fields(answers[item]))
-    print('Ошибки в поле:', type_fields(answers[item]))
-    print()
+# Проход по словарю json
+for i in range(len(answers)):
+    missing_f = list(required_fields.keys())
+    for key, item in answers[i].items():
+        if key in required_fields:
+            if required_fields[key] == 'int':
+                if not type_int(item):
+                    error_fields.append(f'{i}: {key} - Не верный тип')
+            elif required_fields[key] == 'str':
+                if not type_str(item):
+                    error_fields.append(f'{i}: {key} - Не верный тип')
+            elif required_fields[key] == 'bool':
+                if not type_bool(item):
+                    error_fields.append(f'{i}: {key} - Не верный тип')
+            elif required_fields[key] == 'url':
+                if not type_url(item):
+                    error_fields.append(f'{i}: {key} - Ошибка URL')
+            elif required_fields[key] == 'val':
+                if not type_val(item, ['itemBuyEvent', 'itemViewEvent']):
+                    error_fields.append(f'{i}: {key} - Не верное значение')
+        else:
+            extra_fields.append(f'{i}: {key}')
+        if key in missing_f:
+            missing_f.remove(key)
+    if missing_f:
+        for n in missing_f:
+            missing_fields.append(f'{i}: {n}')
+
+# Вывод результата
+if extra_fields or error_fields or missing_fields:
+    print('Fail')
+    print('Отсутствует поле: ', missing_fields)
+    print('Лишнее поле: ', extra_fields)
+    print('Ошибка в поле: ', error_fields)
+else:
+    print('Pass')
